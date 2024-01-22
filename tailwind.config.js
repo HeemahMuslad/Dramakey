5fc005b4ef8508cf99e022f6372e4a5626ca691e
HeemahMuslad <rohimat.m1800276@st.futminna.edu.ng>
2024-01-22 15:48:48 +0100

    Refactor tailwind.config.js screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,9 @@ export default {
   theme: {
     extend: {
       screens: {
-        'lg': {'min': '500px', 'max': '756px'},  // for screens between 756px and 1023px
-        'md': {'min': '380px', 'max': '501px'},   // for screens between 481px and 755px
-        'sm': {'max': '380px'},                    // for screens up to 480px
-      },
+        'lg': {'min': '500px', 'max': '756px'},  
+        'md': {'min': '380px', 'max': '501px'},
+        'sm': {'max': '380px'},                   }
         
     },
    width:{lg:"90%",md:"80%",sm:"20%",xsm:"25px"},
